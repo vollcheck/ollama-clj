839c5cb92b2fd678be8a75eb7861d93d1b8dbaf6
vollcheck <jaw@juxt.pro>
2024-02-14 10:21:17 +0100

    spec for chat, generate, copy and push

--- src/ollama_clj/schema.clj
+++ src/ollama_clj/schema.clj
@@ -15,12 +15,26 @@
    [:role [:enum "user" "system"]]
    [:content nb-string]])
 
-(def ChatOptions
+(def Chat
   [:map
    [:model nb-string]
    [:messages :vector Message]
    [:stream :boolean]
    [:format :string]
+   [:options {:optional true} :any]
+   [:keep-alive {:optional true} :boolean]])
+
+(def Generate
+  [:map
+   [:model nb-string]
+   [:prompt nb-string]
+   [:system {:optional true} :string]
+   [:template {:optional true} :string]
+   [:context {:optional true} :string]
+   [:stream {:optional true} :boolean]
+   [:raw {:optional true} :boolean]
+   [:format {:optional true} :string]
+   [:images {:optional true} :any] ;; TODO
    [:options {:optional true} :any]
    [:keep-alive {:optional true} :boolean]])
 
@@ -44,6 +58,18 @@
    [:prompt nb-string]
    [:?options :vector :string] ;; TODO
    [:?keep-alive :boolean]])
+
+(def Copy
+  [:map
+   [:model nb-string]
+   [:source :string]
+   [:destination :string]])
+
+(def Push
+  [:map
+   [:model nb-string]
+   [:insecure? :boolean]
+   [:stream? :boolean]])
 
 (comment
   (require '[malli.generator :as mg])
