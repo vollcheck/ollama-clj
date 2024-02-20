217826f59eddafb9126b77c956a7b488217784a0
vollcheck <jaw@juxt.pro>
2024-02-20 09:18:48 +0100

    implementing base functions further

--- src/ollama_clj/core.clj
+++ src/ollama_clj/core.clj
@@ -1,4 +1,5 @@
 (ns ollama-clj.core
+  ;; (:exclude [list]) ;; for list
   (:require [clojure.string :as str]
             [aleph.http :as http]
             [manifold.deferred :as d]
@@ -7,13 +8,25 @@
             [ollama-clj.schema :as s]
             [malli.core :as m]))
 
-(defn- resolve-method [method]
-  (ns-resolve 'aleph.http (symbol (name method))))
+(def user-agent-string ;; TODO
+  (format "ollama-clj/%s (%s %s) with Clojure version %s" ollama-version machine os-version (clojure-version)))
+
+(def base-config
+  {:base-url (or (System/getenv "OLLAMA_BASE_URL")
+                 "http://localhost:11434")
+   :follow-redirects? true
+   :timeout nil
+   :headers {"Content-Type" "application/json"
+             "Accept"       "application/json"
+             "User-Agent"   "ollama-clj"}})
 
 (defprotocol BaseClient
   (request        [this method endpoint opts])
   (stream         [this method endpoint opts])
   (request-stream [this method endpoint opts]))
+
+(defn- resolve-method [method]
+  (ns-resolve 'aleph.http (symbol (name method))))
 
 (defrecord Client [^String base-url]
   BaseClient
@@ -41,31 +54,67 @@
       (stream this method endpoint opts)
       (request this method endpoint opts))))
 
+(comment
+  ;; What do you think about instatiating base client for the user?
+  (def base-client (->Client "http://localhost:11434"))
+  )
+
 (defn chat
-  [client model messages opts]
-  (request-stream client
-                  :post
-                  "/api/chat"
-                  (assoc opts
-                         :model model
-                         :messages messages)))
+  ([client model messages]
+   (chat client model messages {}))
+  ([client model messages opts]
+   (request-stream client
+                   :post
+                   "/api/chat"
+                   (assoc opts
+                          :model model
+                          :messages messages))))
 
 (defn generate
-  [client model prompt opts]
-  (request-stream client
-                  :post
-                  "/api/generate"
-                  (assoc opts
-                         :model model
-                         :prompt prompt)))
+  ([client model prompt]
+   (generate client model prompt {:stream? false
+                                  :raw? false}))
+  ([client model prompt opts]
+   (request-stream client
+                   :post
+                   "/api/generate"
+                   (assoc opts
+                          :model model
+                          :prompt prompt))))
 
-(defn- parse-modelfile []
-  (assert false "Not implemented")
-  )
+(defn embeddings
+  ([client model prompt]
+   (embeddings client model prompt {}))
+  ([client model prompt opts]
+   (request client :post "/api/embeddings" (assoc opts :model model :prompt prompt))))
 
-(defn- create-blob []
-  (assert false "Not implemented")
-  )
+(defn pull
+  ([client model]
+   (pull client model {:insecure? false :stream? false}))
+  ([client model opts]
+   (request-stream client :post "/api/pull" (assoc opts :model model))))
+
+(defn push
+  ([client model]
+   (push client model {:insecure? false :stream? false}))
+  ([client model opts]
+   (request-stream client :post "/api/push" (assoc opts :model model))))
+
+(defn create
+  ([client model]
+   (create client model {:stream? false
+                         :path nil
+                         :modelfile nil}))
+  ([client model opts]
+   (request-stream client :post "/api/create" (assoc opts :model model))))
+
+(defn- parse-modelfile
+  ([modelfile] (parse-modelfile modelfile nil))
+  ([modelfile base]
+   (throw (Exception. "Not implemented"))))
+
+(defn- create-blob [path]
+  (throw (Exception. "Not implemented")))
 
 (defn delete [client]
   (let [response (request client :delete "/api/delete" {})]
@@ -73,7 +122,7 @@
       {:status "success"}
       {:status "failure"})))
 
-(defn lst [client]
+(defn list [client]
   (request client :get "/api/tags" {}))
 
 (defn show
@@ -84,27 +133,6 @@
   {:pre [(not (str/blank? model))]}
   (request client :post "/api/show" opts))
 
-;; modelfile='''
-;; FROM llama2
-;; SYSTEM You are mario from super mario bros.
-;; '''
-
-;; ollama.create(model='example', modelfile=modelfile)
-(defn create
-  [client
-   {:keys [model path modelfile stream?]
-    :or {model ""
-         modelfile ""}
-    :as opts}]
-  {:pre [(not (str/blank? model))]}
-  ;; if (realpath := _as_path(path)) and realpath.exists():
-  ;;     modelfile = self._parse_modelfile(realpath.read_text(), base=realpath.parent)
-  ;;   elif modelfile:
-  ;;     modelfile = self._parse_modelfile(modelfile)
-  ;;   else:
-  ;;     raise RequestError('must provide either path or modelfile')
-  (request-stream client :post "/api/create" opts))
-
 (defn copy
   [client opts]
   {:pre [(m/validate s/Copy opts)]}
@@ -112,29 +140,3 @@
     (if (= 200 (:status response))
       {:status "success"}
       {:status "failure"})))
-
-(defn push
-  [client opts]
-  {:pre [(m/validate s/Push opts)]}
-  (request-stream client :post "/api/push" opts))
-
-(defn pull
-  [client
-   {:keys [model insecure? stream?]
-    :or {model ""
-         insecure? false
-         stream? false}
-    :as opts}]
-  {:pre [(not (str/blank? model))]}
-  (request-stream client "/api/pull" opts))
-
-(defn embeddings
-  [client
-   {:keys [model prompt options keep-alive]
-    :or {model ""
-         prompt ""
-         options nil
-         keep-alive nil}
-    :as opts}]
-  {:pre []}
-  (request client "/api/embeddings" opts))
