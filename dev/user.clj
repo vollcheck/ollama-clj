12e687c2ca6c55ee07efbac205e12ffdac03a592
vollcheck <jaw@juxt.pro>
2024-03-02 13:57:46 +0100

    actual examples are in `dev/user.clj` namespace

--- dev/user.clj
+++ dev/user.clj
@@ -3,108 +3,68 @@
    [clojure.string :as str]
    [jsonista.core :as json]
    [ollama-clj.core :as o]
-   [ollama-clj.schema :as s]))
+   [manifold.deferred :as d]
+   [clj-commons.byte-streams :as bs]))
+
+(def kkom json/keyword-keys-object-mapper)
 
 (comment
-  (def port 11434)
-
   (def client (o/->Client "http://localhost:11434"))
 
   (def messages
     [{:role "user"
       :content "Why is the sky blue?"}])
 
-  (def kkom json/keyword-keys-object-mapper)
+  ;; (def response
+  ;;   (o/chat client "stablelm-zephyr" messages {:stream? false}))
 
-  (-> (o/chat client "stablelm-zephyr" messages {:stream? false})
-      deref
-      (json/read-value kkom)
-      (spit "chat.edn")
-      )
-  )
+  ;; simple chain
+  (d/chain (o/chat client "stablelm-zephyr" messages {:stream? false})
+           :body
+           bs/to-string
+           type)
 
-(comment
-  (require '[clojure.string :as str]
-           '[jsonista.core :as json])
+  ;; simple chain with println at the end
+  (d/chain (o/chat client "stablelm-zephyr" messages {:stream? false})
+           :body
+           bs/to-string
+           println)
 
-  (def client (o/->Client "http://localhost:11434"))
+  ;; simple "sync" print
+  (->> @(o/chat client "stablelm-zephyr" messages {:stream? false})
+       :body
+       bs/to-string
+       (spit "chat-response.json")
+       ;; (json/read-value kkom)
+       ;; println
+       )
 
-  (def response
-    (o/generate client "stablelm-zephyr" "Why is the sky blue?"))
+  ;; using the `chat` function with a single message + save to a file
+  (->> @(o/chat client "stablelm-zephyr" "why is the sky blue?" {:stream? false})
+       :body
+       bs/to-string
+       ;; (spit "chat-response.json")
+       ;; (json/read-value kkom)
+       ;; println
+       )
 
-  ;; Response is chunked into separate lines, we need to collect them
-  ;; {:content-type "application/x-ndjson",
-  ;;  :transfer-encoding "chunked"}
-  (def body-lines (-> response :body (str/split-lines)))
-
-  (def kkom json/keyword-keys-object-mapper)
-
-  (->> body-lines
-       (map #(-> % (json/read-value kkom) :response))
-       #_(apply str)
+  (->> @(o/generate client "stablelm-zephyr" "why is the sky blue?" {:stream? false})
+       :body
+       ;;bs/to-string
+       ;; (spit "chat-response.json")
+       ;; (json/read-value kkom)
+       ;; println
        )
 
   )
 
+
 (comment
-  ;; testing the possibiltites of http-kit {:as :stream} option with `/api/generate` endpoitn
-  (require '[jsonista.core :as json]
-           ;; '[org.httpkit.client :as http]
-           '[clojure.java.io :as io]
-           '[clojure.string :as str]
-           '[aleph.http :as http]
-           '[manifold.stream :as s]
-           '[manifold.deferred :as d]
-           '[clj-commons.byte-streams :as bs])
-
-  (def kkom json/keyword-keys-object-mapper)
-
-  (def body
-    (-> {:model "stablelm-zephyr"
-         :prompt "Why is the sky blue?"}
-        (json/write-value-as-string)))
-
-  (def raw-stream-connection-pool (http/connection-pool {:connection-options {:raw-stream? true}}))
-  ;; apparently, this is the right way to do the streaming, what form should I return the response in for people
-  ;; to be able to use it in the same streaming manner?
-  (defn using-aleph-manifold-streaming []
-    (-> (http/post "http://localhost:11434/api/generate"
-                   {:body body
-                    :pool raw-stream-connection-pool})
-        (d/chain (fn [{:keys [body] :as _resp}]
-                   (s/consume
-                    (fn [chunk]
-                      (print "_" (-> chunk bs/to-string (json/read-value kkom) :response)))
-                    body)))))
-
-  (using-aleph-manifold-streaming)
-
-
-  ;; using input coercion `{:as :stream}`
-  (defn using-coercion []
-    (with-open [rdr (-> (http/post "http://localhost:11434/api/generate"
-                                   {:body body
-                                    :as :stream}) ;; => org.httpkit.BytesInputStream
-                        deref
-                        :body
-                        (io/reader #_#_:encoding "UTF-8"))]
-      (doseq [line (line-seq rdr)]
-        (print (-> line (json/read-value kkom) :response)))))
-
-  (using-coercion)
-
-
-  ;; using callback, async?
-  (defn using-callback []
-    (http/post "http://localhost:11434/api/generate"
-               {:body body
-                #_#_:as :stream}
-               (fn [{:keys [error body] :as _resp}]
-                 (if error
-                   (println "Error: " error)
-                   (doseq [chunk body]
-                     (print "chunk")
-                     (print (String. chunk)))))))
-
-  (using-callback)
+  "embeddings test"
+  (def client (o/->Client "http://localhost:11434"))
+  (-> (o/embeddings client "stablelm-zephyr" "Why is the sky blue?")
+      deref
+      (json/read-value kkom) ;; TODO test further JSON parsing
+      type
+      )
   )
