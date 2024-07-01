00404f460fd187e6c8d6e167bfb73e1c66e3fba6
vollcheck <jaw@juxt.pro>
2024-07-01 22:20:41 +0200

    dev namespace as a good testing playground

--- dev/user.clj
+++ dev/user.clj
@@ -1,23 +1,55 @@
 (ns user
-  (:require
-   [clojure.string :as str]
-   [jsonista.core :as json]
-   [ollama-clj.core :as o]
-   [manifold.deferred :as d]
-   [clj-commons.byte-streams :as bs]
-   [manifold.stream :as s]))
+  (:require [jsonista.core :as json]
 
-(def kkom json/keyword-keys-object-mapper)
+            [manifold.deferred :as d]
+            [clj-commons.byte-streams :as bs]
+            [manifold.stream :as s]
+
+            [ollama-clj.core :as o]
+            [ollama-clj.util :as u]))
+
 
 (comment
-  (def client (o/->Client "http://localhost:11434"))
+  (def kkom json/keyword-keys-object-mapper)
+
+  (def client (o/make-client "http://localhost:11434"
+                             "stablelm-zephyr"))
 
   (def messages
     [{:role "user"
       :content "Why is the sky blue? Please keep the answer short."}])
 
+  (def prompt "Why is the sky blue? Please keep the answer short.")
+
+  (def model "stablelm-zephyr")
+
+  (doseq [part (deref (u/process (o/chat client messages)))]
+    (println part))
+
+  (doseq [part (deref (u/process (o/generate client model prompt)))]
+    (println part))
+
+
+  ;;;;;; SHOW TESTS
+  ;; NOTE: this should raise an error says the model is not found
+  (o/show (o/make-client "http://localhost:11434" nil))
+
+  ;; NOTE: good, uses model defined in client
+  (o/show (o/make-client "http://localhost:11434" "stablelm-zephyr"))
+
+  ;; NOTE: good, but raw output (manifold deferred)
+  (o/show (o/make-client "http://localhost:11434" "stablelm-zephyr")
+          {:raw? true})
+
+  ;; NOTE: good, same as above but with raw output
+  (o/show client {:raw? true})
+
+  ;; TODO: it raises 404 error
+  (o/show client {:model "mistral"})
+
+
   ;; sync
-  (->> (d/chain (o/chat client #_"stablelm-zephyr" "mistral" messages)
+  (->> (d/chain (o/chat client "stablelm-zephyr" messages)
                 :body
                 bs/to-line-seq
                 #(s/map (fn [x] (-> x
@@ -44,6 +76,7 @@
                 )
        deref)
 
+  )
 
 (comment
   "embeddings test"
